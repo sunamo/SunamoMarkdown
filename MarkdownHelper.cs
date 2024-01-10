ceced17f0bbb0e2dc6cea8e86ac4461b3810abe3
sunamo <radek.jancik@sunamo.cz>
2024-01-10 11:49:49 +0100

    .

---
+++ MarkdownHelper.cs
@@ -0,0 +1,63 @@
+using SunamoHtml;
+using SunamoHtml.Html;
+using SunamoStringReplace;
+using TextCopy;
+
+namespace SunamoMarkdown;
+
+public class MarkdownHelper
+{
+    static Type type = typeof(MarkdownHelper);
+
+    /// <summary>
+    /// Uses Html2Markdown which has dependency HtmlAgilityPack 1.5
+    /// Therefore I Cant replace with 1s standard 1.11.2 and cant compile these project
+    /// Therefore commented and remove nuget package
+    /// </summary>
+    /// <param name="html"></param>
+    public static string ConvertToMarkDown(string html)
+    {
+        var converter = new Converter();
+        var markdown = converter.Convert(html);
+        return markdown;
+    }
+
+    public static string ConvertToMarkDownMy(string input)
+    {
+        dynamic hd = null;
+        hd = HtmlAgilityHelper.CreateHtmlDocument();
+        hd.LoadHtml(input);
+
+        dynamic nodes = null;
+        nodes = HtmlAgilityHelper.Nodes(hd.DocumentNode, true, "*");
+        HtmlHelper.DeleteAttributesFromAllNodes(nodes);
+
+        input = hd.DocumentNode.OuterHtml;
+
+        input = ReplacePairTag(input, "bold", "**");
+        input = ReplacePairTag(input, "strong", "**");
+        input = ReplacePairTag(input, "b", "**");
+        input = ReplacePairTag(input, "i", "_");
+        input = ReplacePairTag(input, "strike", "-");
+        input = HtmlHelper.StripAllTags(input);
+
+        input = SHReplace.ReplaceWhiteSpaces(input, " ");
+        input = SHReplace.ReplaceAllDoubleSpaceToSingle2(input);
+
+
+
+        input = input.Trim();
+
+        ClipboardService.SetText(input);
+
+        return input;
+    }
+
+    public static string ReplacePairTag(string input, string tag, string forWhat)
+    {
+        input = input.Replace("<" + tag + ">", forWhat);
+        input = input.Replace("<" + tag + " ", forWhat);
+        input = input.Replace("</" + tag + ">", forWhat);
+        return input;
+    }
+}
