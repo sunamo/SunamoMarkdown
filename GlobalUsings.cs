e04e0003461503c0c709f0becd1289b2d42c07cb
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-16 09:11:46 +0200

    Removed central classes with consts

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -5,7 +5,7 @@ global using System.Collections.Generic;
 global using System.Collections;
 global using System.IO;
 global using System.Linq;
-global using SunamoMarkdown._sunamo.SunamoValues;
+global using SunamoMarkdown;
 global using SunamoMarkdown._sunamo.SunamoExceptions;
 global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
